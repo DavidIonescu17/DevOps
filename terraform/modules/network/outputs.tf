7e7a8ceb7c03b196515c0b8331af8c89a60fff31
DavidIonescu17 <davidionescu365@gmail.com>
2026-04-03 21:48:22 +0300

    fix: vnet resource name in outputs

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -6,6 +6,6 @@ output "subnet_ids" {
 }
 
 output "vnet_id" {
-  value       = azurerm_virtual_network.this.id
+  value       = azurerm_virtual_network.vnet.id
   description = "ID of the Virtual Network"
 }
