e37a6c810f4f50ada004f1ae3d37301aef5f0d0a
DavidIonescu17 <davidionescu365@gmail.com>
2026-04-03 21:47:37 +0300

    fix: deleted unnecessary output in network module

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,19 +1,11 @@
 output "subnet_ids" {
   description = "Map of subnet IDs (non-delegated)"
   value = {
-    for k, v in azurerm_subnet.subnets : k => v.id
+    for k, v in azurerm_subnet.subnet : k => v.id
   }
 }
 
 output "vnet_id" {
   value       = azurerm_virtual_network.this.id
   description = "ID of the Virtual Network"
-}
-
-
-output "delegated_subnet_ids" {
-  description = "Map of subnet IDs (delegated"
-  value = {
-    for k, v in azurerm_subnet.subnets_with_delegation : k => v.id
-  }
 }
