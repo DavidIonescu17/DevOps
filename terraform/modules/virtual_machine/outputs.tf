1a7e43b04114914645ee1be41bfc86b2dbf16c12
Ionescu David <davidionescu365@gmail.com>
2026-05-01 16:28:05 +0300

    fix: update output references for virtual machine module in outputs.tf

--- terraform/modules/virtual_machine/outputs.tf
+++ terraform/modules/virtual_machine/outputs.tf
@@ -1,7 +1,15 @@
 output "vm_id" {
-  value = azurerm_linux_virtual_machine.this.id
+  value = azurerm_linux_virtual_machine.vm.id
 }
 
 output "public_ip_address" {
-  value = azurerm_public_ip.vm_public_ip.ip_address
+  value = var.create_public_ip ? azurerm_public_ip.vm_public_ip[0].ip_address : null
+}
+
+output "private_ip_address" {
+  value = azurerm_network_interface.vm_nic.private_ip_address
+}
+
+output "nic_id" {
+  value = azurerm_network_interface.vm_nic.id
 }
