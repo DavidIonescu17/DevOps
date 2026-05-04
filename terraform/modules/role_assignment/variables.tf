d4cc08ac43ab39f25962a538a7ad15f900a0f36e
Ionescu David <davidionescu365@gmail.com>
2026-05-04 15:44:55 +0300

    fix: update role_assignment module to use role_definition_name instead of role_definition_id

--- terraform/modules/role_assignment/variables.tf
+++ terraform/modules/role_assignment/variables.tf
@@ -2,7 +2,7 @@ variable "role_config" {
   description = "Map of role assignment names to their configuration"
   type = map(object({
     scope              = string
-    role_definition_id = string
+    role_definition_name = string
     principal_id       = string
   }))
 }
